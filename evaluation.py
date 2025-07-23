1570977efb44868de3f09c3c15d248f87229aa5a
Prathamesh Santosh Mahadik <patu3137@gmail.com>
2025-07-23 19:46:03 +0530

    new file is created

---
+++ evaluation.py
@@ -0,0 +1,84 @@
+# checking number is zero or not
+# x=input("enter any number:")
+# if(x==0):
+#     print("given number is zero")
+# else:
+#     print("given number is non zero")
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# finding greatest of two numbers
+# x1=int(input("enter the value of 1st number:"))
+# x2=int(input("enter the value of 2nd number:"))
+
+# if(x1>x2):
+#     print(f"{x1} is greatest number!!")
+# elif(x1<x2):
+#      print(f"{x2} is greatest number!!")
+# else:
+#     print("both the numbers are equal!!")
+
+# ------------------------------------------------------------------------------------------------------------------
+
+#checking number is positive or negative
+# x1=int(input("enter the number :"))
+# if x>0:
+#     print("given number is positive")
+# elif x<0:
+#     print("given number is not positive")
+# else:
+#     print("given number is zero")
+
+# ------------------------------------------------------------------------------------------------------------------
+#checking the input character is vowel or consonants
+
+# x=str(input("input a single character:"))
+
+# if x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u':
+#     print("input character is vowel")
+# else :
+#     print("input alphabet is consonants")
+
+# ------------------------------------------------------------------------------------------------------------------
+
+#result of students
+
+# x=int(input("enter the marks of student in percentage"))
+
+# if x>=90:
+#     print("Excellent performance:")
+# elif x>=80:
+#     print("Very good performance:")
+# elif x>=70:
+#     print("good performance")
+# elif x>=60:
+#     print("average performance")
+# else:
+#     print("poor performance")
+
+# ------------------------------------------------------------------------------------------------------------------
+
+#Largest of three numbers
+
+# x1=int(input("Enter the 1st value "))
+# x2=int(input("Enter the 2nd value"))
+# x3=int(input("enter 3rd value"))
+
+# if x1>x2 and x1>x3:
+#     print(f"{x1} is greatest number:")
+# elif x2>x3 and x2>x3:
+#     print(f"{x2}is largest number:")
+    
+# elif x3>x1 and x3>x1:
+#     print(f"{x3} is largest number:")
+
+# else:
+#     print("enter valid number:")
+
+# ------------------------------------------------------------------------------------------------------------------
+
+x=int(input("enter a number:"))
+
+while i in x:
+    print(i)
+    i+=1
