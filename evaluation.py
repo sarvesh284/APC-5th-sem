e5973dbfb206682d0b3a534d381a224f0d0014e3
Prathamesh Santosh Mahadik <patu3137@gmail.com>
2025-07-30 15:08:17 +0530

    basic codes are done.

--- evaluation.py
+++ evaluation.py
@@ -77,8 +77,75 @@
 
 # ------------------------------------------------------------------------------------------------------------------
 
-x=int(input("enter a number:"))
+# x=int(input("enter a number:"))
+# i=0
+# for i in range(x):
+#     print(i)
+#     i+=1
+#------------------------------------------------------------------------------------------------------------------
 
-while i in x:
-    print(i)
-    i+=1
+# x=int(input("enter a number:"))
+# i=0
+# for i in range(0,x+1,2):
+#     print(i)
+#------------------------------------------------------------------------------------------------------------------
+
+# n=int(input("enter the number to calculate sum:"))
+# i=0
+# sum=0
+# for i in range(n):
+#     sum+=i
+#     i+=1
+# print(sum)
+#------------------------------------------------------------------------------------------------------------------
+# n=int(input("enter the number to calculate sum:"))
+# i=0
+# sum=0
+# for i in range(0,n,2):
+#     print(i)
+#     sum+=i
+#     i+=1
+# print(sum)
+#------------------------------------------------------------------------------------------------------------------
+#problem statement 
+#write a program to build simple text analysis tool the tool should perform following task for the input text
+# 1.count total number of words
+# 2.count the no.of spaces
+# 3.count the frequency of each word from input then identify and display top 3 mostly frequent word
+# 4.count the number of vowels in the entire text
+# 5.sort the string with conversion of original string into the reverse ascending order
+
+
+#problem statement
+#write a program to analyze two different paragraphs where
+#you need to do followimg task
+#1.find all unique words use in each paragraph
+#2.identify the common words between both paragraphs.
+#3.display total count of unique words in both paragraphs
+
+#------------------------------------------------------------------------------------------------------------------
+# n=int(input("enter the value of n:"))
+# while (n>=0):
+#         print(n)
+#         n-=1
+#------------------------------------------------------------------------------------------------------------------
+# n=int(input("enter a number for printing fibonacci series upto:"))
+# i=0
+# t1=0
+# t2=1
+# while(i<n):
+#         t3=t1+t2
+#         print(t3)
+#         t1=t2
+#         t2=t3
+#         i+=1
+
+#------------------------------------------------------------------------------------------------------------------
+# n=int(input("enter the number to calculate it's factorial:"))
+# i=1                             #in factorial i should always initialize to 1
+# fact=1
+# while(i<=n):
+#     fact*=i
+#     i+=1
+# print(fact)
+#------------------------------------------------------------------------------------------------------------------
